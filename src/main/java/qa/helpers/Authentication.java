093fb9329b6e12359ca42f243a0442de919f81f8
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-11 22:52:50 +0100

    Added reading credentials from the Credentials class

--- src/main/java/qa/helpers/Authentication.java
+++ src/main/java/qa/helpers/Authentication.java
@@ -1,15 +1,19 @@
 package qa.helpers;
 
 import io.appium.java_client.android.AndroidDriver;
+import qa.json.JSONReader;
 import qa.pageobject.loginview.LoginView;
+import qa.utils.Credentials;
 
 public class Authentication {
 
     public static void perform(AndroidDriver driver) {
 
+        Credentials[] credentials = JSONReader.getCredentials("correct");
+
         LoginView loginView = new LoginView(driver);
-        loginView.setUsername("company");
-        loginView.setPassword("company");
+        loginView.setUsername(credentials[0].getUsername());
+        loginView.setPassword(credentials[0].getPassword());
         loginView.tapLoginButton();
     }
 }
