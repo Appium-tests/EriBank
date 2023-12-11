2ba415228ba3d58a24b2c9b439936f84462f2f4a
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-11 10:49:15 +0100

    Created the BaseTest class

---
+++ src/main/java/qa/base/BaseTest.java
@@ -0,0 +1,35 @@
+package qa.base;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import qa.driver.Driver;
+import qa.json.JSONReader;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public class BaseTest {
+
+    private AndroidDriver driver;
+
+    @BeforeClass
+    public void readJSON() throws IOException {
+        JSONReader.read();
+    }
+
+    @BeforeMethod
+    public void setUp() throws MalformedURLException {
+        driver = Driver.createDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    protected AndroidDriver getDriver() {
+        return driver;
+    }
+}
