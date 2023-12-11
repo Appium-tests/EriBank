b85baf0a99c6eef233c71f12b121eb2c59251991
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-11 10:23:39 +0100

    Added getCredentials(String node) method

--- src/main/java/qa/json/JSONReader.java
+++ src/main/java/qa/json/JSONReader.java
@@ -3,10 +3,12 @@ package qa.json;
 import com.google.common.io.Resources;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import qa.utils.Credentials;
 
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.IntStream;
 
 public class JSONReader {
 
@@ -25,5 +27,15 @@ public class JSONReader {
         jsonObject = new JSONObject(Resources.toString(url, StandardCharsets.UTF_8));
     }
 
+    public static Credentials[] getCredentials(String node) {
 
+        JSONArray jsonArray = getJSONArray("credentials", node);
+
+        return IntStream.range(0, jsonArray.length())
+                .mapToObj(i -> new Credentials(
+                        jsonArray.getJSONObject(i).getString("username"),
+                        jsonArray.getJSONObject(i).getString("password")
+                ))
+                .toArray(Credentials[]::new);
+    }
 }
