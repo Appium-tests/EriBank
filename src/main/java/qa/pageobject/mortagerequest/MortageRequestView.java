bfbb2a150cb99f4573f5166d329eedf7f8f6124b
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-11 22:14:07 +0100

    Created the MortageRequestView class

---
+++ src/main/java/qa/pageobject/mortagerequest/MortageRequestView.java
@@ -0,0 +1,24 @@
+package qa.pageobject.mortagerequest;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
+import qa.base.BaseView;
+
+import java.util.List;
+
+public class MortageRequestView extends BaseView {
+
+    public MortageRequestView(AndroidDriver driver) {
+
+        super(driver);
+    }
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/makePaymentView")
+    List<WebElement> parent;
+
+    public boolean isDisplayed() {
+
+        return !parent.isEmpty();
+    }
+}
