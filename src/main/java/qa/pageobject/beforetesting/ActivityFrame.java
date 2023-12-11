1ae981bccd73b93b394c8c0d99fbb65a4b415b4a
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-11 10:55:28 +0100

    Created the ActivityFrame class

---
+++ src/main/java/qa/pageobject/beforetesting/ActivityFrame.java
@@ -0,0 +1,30 @@
+package qa.pageobject.beforetesting;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
+import qa.base.BaseView;
+
+public class ActivityFrame extends BaseView {
+    public ActivityFrame(AndroidDriver driver) {
+        super(driver);
+    }
+
+    @AndroidFindBy(id = "\"android:id/switch_widget\"")
+    WebElement switchButton;
+
+    @AndroidFindBy(id = "com.android.permissioncontroller:id/continue_button")
+    WebElement continueButton;
+
+    public boolean isSwitchButtonOn() {
+        return switchButton.getAttribute("checked").equals("true");
+    }
+
+    public void tapSwitchButton() {
+        switchButton.click();
+    }
+
+    public void tapContinueButton() {
+        continueButton.click();
+    }
+}
