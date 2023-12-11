47baa2228998253f4aa249246aa5bb2b7368a67f
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-11 10:41:16 +0100

    Created the Driver class

---
+++ src/main/java/qa/driver/Driver.java
@@ -0,0 +1,33 @@
+package qa.driver;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.nio.file.Paths;
+
+public class Driver {
+
+    private static final URI uri = Paths.get("http://127.0.0.1:4723/").toUri();
+
+    private static DesiredCapabilities getCapabilities() {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("automationName", "uiautomator2");
+        capabilities.setCapability(MobileCapabilityType.UDID, "MyUDID");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.ExperiBank");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".LoginActivity");
+        capabilities.setCapability("appium:autoGrantPermission", true);
+
+        return capabilities;
+    }
+
+    private AndroidDriver createDriver() throws MalformedURLException {
+
+        return new AndroidDriver(uri.toURL(), getCapabilities());
+    }
+}
