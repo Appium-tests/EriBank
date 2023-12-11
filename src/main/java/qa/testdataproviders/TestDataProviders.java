b464e498e9b7753c9f32abc73e2c9321ddfe2468
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-11 10:30:21 +0100

    Created the TestDataProviders class and methods

---
+++ src/main/java/qa/testdataproviders/TestDataProviders.java
@@ -0,0 +1,37 @@
+package qa.testdataproviders;
+
+import org.testng.annotations.DataProvider;
+import qa.json.JSONReader;
+
+public class TestDataProviders {
+
+    @DataProvider(name = "CR_correct")
+    public Object[] CR_correct() {
+
+        return JSONReader.getCredentials("correct");
+    }
+
+    @DataProvider(name = "CR_incorrectUsername")
+    public Object[] CR_incorrectUsername() {
+
+        return JSONReader.getCredentials("incorrectUsername");
+    }
+
+    @DataProvider(name = "CR_blankUsernameField")
+    public Object[] CR_blankUsernameField() {
+
+        return JSONReader.getCredentials("blankUsernameField");
+    }
+
+    @DataProvider(name = "CR_incorrectPassword")
+    public Object[] CR_incorrectPassword() {
+
+        return JSONReader.getCredentials("incorrectPassword");
+    }
+
+    @DataProvider(name = "CR_blankPasswordField")
+    public Object[] CR_blankPasswordField() {
+
+        return JSONReader.getCredentials("blankPasswordField");
+    }
+}
