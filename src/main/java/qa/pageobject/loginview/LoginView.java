be3fd573f915e85aab42599afd78a1629af77dc9
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-11 18:30:42 +0100

    Created the LoginView class

---
+++ src/main/java/qa/pageobject/loginview/LoginView.java
@@ -0,0 +1,37 @@
+package qa.pageobject.loginview;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
+import qa.base.BaseView;
+
+public class LoginView extends BaseView {
+    public LoginView(AndroidDriver driver) {
+
+        super(driver);
+    }
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/usernameTextField")
+    WebElement usernameField;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/passwordTextField")
+    WebElement passwordField;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/loginButton")
+    WebElement loginButton;
+
+    public void setUsername(String username) {
+
+        usernameField.sendKeys(username);
+    }
+
+    public void setPassword(String password) {
+
+        passwordField.sendKeys(password);
+    }
+
+    public void tapLoginButton() {
+
+        loginButton.click();
+    }
+}
