d458612059c649ca29e12200360cc91b0ac4323b
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-11 22:54:55 +0100

    Added List<WebElement> parent and boolean method

--- src/main/java/qa/pageobject/loginview/LoginView.java
+++ src/main/java/qa/pageobject/loginview/LoginView.java
@@ -5,6 +5,8 @@ import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.openqa.selenium.WebElement;
 import qa.base.BaseView;
 
+import java.util.List;
+
 public class LoginView extends BaseView {
 
     private final AlertFrame alertFrame;
@@ -15,6 +17,9 @@ public class LoginView extends BaseView {
         alertFrame = new AlertFrame(driver);
     }
 
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/loginView")
+    List<WebElement> parent;
+
     @AndroidFindBy(id = "com.experitest.ExperiBank:id/usernameTextField")
     WebElement usernameField;
 
@@ -23,6 +28,11 @@ public class LoginView extends BaseView {
 
     @AndroidFindBy(id = "com.experitest.ExperiBank:id/loginButton")
     WebElement loginButton;
+
+    public boolean isDisplayed() {
+
+        return !parent.isEmpty();
+    }
 
     public void setUsername(String username) {
 
