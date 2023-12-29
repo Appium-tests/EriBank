4e422e67532cb7e4f960b052d97747a8053e12c8
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-29 18:29:52 +0100

    Added locators and methods

--- src/main/java/qa/pageobject/makepaymentview/MakePaymentView.java
+++ src/main/java/qa/pageobject/makepaymentview/MakePaymentView.java
@@ -17,6 +17,54 @@ public class MakePaymentView extends BaseView {
     @AndroidFindBy(id = "com.experitest.ExperiBank:id/makePaymentView")
     List<WebElement> parent;
 
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/phoneTextField")
+    WebElement phoneField;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/nameTextField")
+    WebElement nameField;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/amountTextField")
+    WebElement amountField;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/countryTextField")
+    WebElement countryField;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/sendPaymentButton")
+    WebElement sendPaymentButton;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/cancelButton")
+    WebElement cancelButton;
+
+    public void setPhone(String phone) {
+
+        phoneField.sendKeys(phone);
+    }
+
+    public void setName(String name) {
+
+        nameField.sendKeys(name);
+    }
+
+    public void setAmount(String amount) {
+
+        amountField.sendKeys(amount);
+    }
+
+    public void setCountry(String country) {
+
+        countryField.sendKeys(country);
+    }
+
+    public void tapSendPaymentButton() {
+
+        sendPaymentButton.click();
+    }
+
+    public void tapCancelButton() {
+
+        cancelButton.click();
+    }
+
     public boolean isDisplayed() {
 
         return !parent.isEmpty();
