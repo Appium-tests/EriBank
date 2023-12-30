723499aef05173de0600fa3668ed21c311c6cb72
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-30 08:36:35 +0100

    Added fields

--- src/main/java/qa/utils/Payment.java
+++ src/main/java/qa/utils/Payment.java
@@ -9,12 +9,16 @@ public class Payment {
     private final String name;
     private final String amount;
     private final String country;
+    private final String alertTitle;
+    private final String alertMessage;
 
-    public Payment(String phone, String name, String amount, String country) {
+    public Payment(String phone, String name, String amount, String country, String alertTitle, String alertMessage) {
 
         this.phone = phone;
         this.name = name;
         this.amount = amount;
         this.country = country;
+        this.alertTitle = alertTitle;
+        this.alertMessage = alertMessage;
     }
 }
