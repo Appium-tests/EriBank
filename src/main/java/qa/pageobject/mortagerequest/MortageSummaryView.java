780941fc46d15c215949c2eb1352412689dc7728
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-31 11:52:04 +0100

    Created the MortageSummaryView class

---
+++ src/main/java/qa/pageobject/mortagerequest/MortageSummaryView.java
@@ -0,0 +1,34 @@
+package qa.pageobject.mortagerequest;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
+import qa.base.BaseView;
+
+import java.util.List;
+
+public class MortageSummaryView extends BaseView {
+
+    public MortageSummaryView(AndroidDriver driver) {
+
+        super(driver);
+    }
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/typeOfLoanListView")
+    List<WebElement> typeOfLoanListView;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/numberOfYearsListView")
+    List<WebElement> numberOfYearsListView;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/typeOfOccupationListView")
+    List<WebElement> typeOfOccupationListView;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/yearlyIncomeListView")
+    List<WebElement> yearlyIncomeListView;
+
+    public boolean isDisplayed() {
+
+        return !typeOfLoanListView.isEmpty() && !numberOfYearsListView.isEmpty() &&
+               !typeOfOccupationListView.isEmpty() && !yearlyIncomeListView.isEmpty();
+    }
+}
