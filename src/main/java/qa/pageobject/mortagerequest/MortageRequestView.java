583ecf82710ad2bbc55d36f81caa6a3c4d778b6a
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-31 07:46:39 +0100

    Added locators and methods

--- src/main/java/qa/pageobject/mortagerequest/MortageRequestView.java
+++ src/main/java/qa/pageobject/mortagerequest/MortageRequestView.java
@@ -17,6 +17,70 @@ public class MortageRequestView extends BaseView {
     @AndroidFindBy(id = "com.experitest.ExperiBank:id/makePaymentView")
     List<WebElement> parent;
 
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/nameTextField")
+    WebElement firstNameField;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/nameTextField")
+    WebElement lastNameField;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/ageTextField")
+    WebElement ageField;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/addressOneTextField")
+    WebElement addressField1;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/addressTwoTextField")
+    WebElement addressField2;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/countryTextField")
+    WebElement countryField;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/nextButton")
+    WebElement nextButton;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/cancelButton")
+    WebElement cancelButton;
+
+    public void setFirstName(String firstName) {
+
+        firstNameField.sendKeys(firstName);
+    }
+
+    public void setLastName(String lastName) {
+
+        lastNameField.sendKeys(lastName);
+    }
+
+    public void setAge(String age) {
+
+        ageField.sendKeys(age);
+    }
+
+    public void setAddress1(String address) {
+
+        addressField1.sendKeys(address);
+    }
+
+    public void setAddressField2(String address) {
+
+        addressField2.sendKeys(address);
+    }
+
+    public void setCountry(String country) {
+
+        countryField.sendKeys(country);
+    }
+
+    public void tapNextButton() {
+
+        nextButton.click();
+    }
+
+    public void tapCancelButton() {
+
+        cancelButton.click();
+    }
+
     public boolean isDisplayed() {
 
         return !parent.isEmpty();
