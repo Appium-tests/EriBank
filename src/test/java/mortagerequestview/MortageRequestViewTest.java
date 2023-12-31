74f451770dad6e278691149834b8a408e77f8c10
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-31 11:51:04 +0100

    Changed selector id

--- src/test/java/mortagerequestview/MortageRequestViewTest.java
+++ src/test/java/mortagerequestview/MortageRequestViewTest.java
@@ -1,17 +1,21 @@
 package mortagerequestview;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
 import qa.helpers.Authentication;
+import qa.helpers.TestHelper;
 import qa.pageobject.homeview.HomeView;
 import qa.pageobject.mortagerequest.MortageRequestView;
+import qa.pageobject.mortagerequest.MortageSummaryView;
 import qa.testdataproviders.TestDataProviders;
 import qa.utils.MortageRequest;
 
 public class MortageRequestViewTest extends BaseTest {
 
     private MortageRequestView mortageRequestView;
+    private TestHelper testHelper;
 
     @BeforeMethod
     public void create() {
@@ -19,6 +23,9 @@ public class MortageRequestViewTest extends BaseTest {
         Authentication.perform(getDriver());
         HomeView homeView = new HomeView(getDriver());
         homeView.tapMortageRequestButton();
+
+        mortageRequestView = new MortageRequestView(getDriver());
+        testHelper = new TestHelper();
     }
 
     private void fill(MortageRequest mortageRequest) {
@@ -31,9 +38,94 @@ public class MortageRequestViewTest extends BaseTest {
         mortageRequestView.setCountry(mortageRequest.getCountry());
     }
 
+    private void checkWhenDataIsCorrect() {
+
+        MortageSummaryView mortageSummaryView = new MortageSummaryView(getDriver());
+
+        Assert.assertTrue(mortageSummaryView.isDisplayed());
+    }
+
     @Test(dataProvider = "MR_correct", dataProviderClass = TestDataProviders.class)
     public void correct(MortageRequest mortageRequest) {
 
+        fill(mortageRequest);
+        checkWhenDataIsCorrect();
+    }
 
+    @Test(dataProvider = "MR_blankSecondAddressField", dataProviderClass = TestDataProviders.class)
+    public void blankSecondAddressField(MortageRequest mortageRequest) {
+
+        fill(mortageRequest);
+        checkWhenDataIsCorrect();
+    }
+
+    @Test(dataProvider = "MR_incorrectFirstName", dataProviderClass = TestDataProviders.class)
+    public void incorrectFirstName(MortageRequest mortageRequest) {
+
+        fill(mortageRequest);
+        testHelper.checkWhenDataIsIncorrect(mortageRequestView.getAlertFrame(), mortageRequest.getTitle(), mortageRequest.getMessage());
+    }
+
+    @Test(dataProvider = "MR_blankFirstNameField", dataProviderClass = TestDataProviders.class)
+    public void blankFirstNameField(MortageRequest mortageRequest) {
+
+        fill(mortageRequest);
+        testHelper.checkWhenFieldIsBlank(mortageRequestView.isNextButtonEnabled(), "Next");
+    }
+
+    @Test(dataProvider = "MR_incorrectLastName", dataProviderClass = TestDataProviders.class)
+    public void incorrectLastName(MortageRequest mortageRequest) {
+
+        fill(mortageRequest);
+        testHelper.checkWhenDataIsIncorrect(mortageRequestView.getAlertFrame(), mortageRequest.getTitle(), mortageRequest.getMessage());
+    }
+
+    @Test(dataProvider = "MR_blankLastNameField", dataProviderClass = TestDataProviders.class)
+    public void blankBlankNameField(MortageRequest mortageRequest) {
+
+        fill(mortageRequest);
+        testHelper.checkWhenFieldIsBlank(mortageRequestView.isNextButtonEnabled(), "Next");
+    }
+
+    @Test(dataProvider = "MR_incorrectAge", dataProviderClass = TestDataProviders.class)
+    public void incorrectAge(MortageRequest mortageRequest) {
+
+        fill(mortageRequest);
+        testHelper.checkWhenDataIsIncorrect(mortageRequestView.getAlertFrame(), mortageRequest.getTitle(), mortageRequest.getMessage());
+    }
+
+    @Test(dataProvider = "MR_blankAgeField", dataProviderClass = TestDataProviders.class)
+    public void blankAgeField(MortageRequest mortageRequest) {
+
+        fill(mortageRequest);
+        testHelper.checkWhenFieldIsBlank(mortageRequestView.isNextButtonEnabled(), "Next");
+    }
+
+    @Test(dataProvider = "MR_incorrectAddress", dataProviderClass = TestDataProviders.class)
+    public void incorrectAddress(MortageRequest mortageRequest) {
+
+        fill(mortageRequest);
+        testHelper.checkWhenDataIsIncorrect(mortageRequestView.getAlertFrame(), mortageRequest.getTitle(), mortageRequest.getMessage());
+    }
+
+    @Test(dataProvider = "MR_blankAddressField", dataProviderClass = TestDataProviders.class)
+    public void blankAddressField(MortageRequest mortageRequest) {
+
+        fill(mortageRequest);
+        testHelper.checkWhenFieldIsBlank(mortageRequestView.isNextButtonEnabled(), "Next");
+    }
+
+    @Test(dataProvider = "MR_incorrectCountry", dataProviderClass = TestDataProviders.class)
+    public void incorrectCountry(MortageRequest mortageRequest) {
+
+        fill(mortageRequest);
+        testHelper.checkWhenDataIsIncorrect(mortageRequestView.getAlertFrame(), mortageRequest.getTitle(), mortageRequest.getMessage());
+    }
+
+    @Test(dataProvider = "MR_blankCountryField", dataProviderClass = TestDataProviders.class)
+    public void blankCountryField(MortageRequest mortageRequest) {
+
+        fill(mortageRequest);
+        testHelper.checkWhenFieldIsBlank(mortageRequestView.isNextButtonEnabled(), "Next");
     }
 }
