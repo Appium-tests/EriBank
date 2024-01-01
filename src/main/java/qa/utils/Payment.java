e959ed0415d02cb5606973c9b6caf838fa4edc12
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-01 10:52:19 +0100

    Added the String question field

--- src/main/java/qa/utils/Payment.java
+++ src/main/java/qa/utils/Payment.java
@@ -9,15 +9,17 @@ public class Payment {
     private final String name;
     private final String amount;
     private final String country;
+    private final String question;
     private final String title;
     private final String message;
 
-    public Payment(String phone, String name, String amount, String country, String title, String message) {
+    public Payment(String phone, String name, String amount, String country, String question, String title, String message) {
 
         this.phone = phone;
         this.name = name;
         this.amount = amount;
         this.country = country;
+        this.question = question;
         this.title = title;
         this.message = message;
     }
