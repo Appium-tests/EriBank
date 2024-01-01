fc343f78c6023839009fb9d827edbb6c74d993cb
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-01 23:47:04 +0100

    Added locators and methods

--- src/main/java/qa/pageobject/expensereportview/ExpenseReportView.java
+++ src/main/java/qa/pageobject/expensereportview/ExpenseReportView.java
@@ -17,6 +17,35 @@ public class ExpenseReportView extends BaseView {
     @AndroidFindBy(id = "com.experitest.ExperiBank:id/makePaymentView")
     List<WebElement> parent;
 
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/addButton")
+    WebElement addButton;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/backButton")
+    WebElement backButton;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/titleTextView")
+    List<WebElement> expenseTitles;
+
+    public void tapAddButton() {
+
+        addButton.click();
+    }
+
+    public void tapBackButton() {
+
+        backButton.click();
+    }
+
+    public int getCount() {
+
+        return expenseTitles.size();
+    }
+
+    public void tapOnRow(int row) {
+
+        expenseTitles.get(row).click();
+    }
+
     public boolean isDisplayed() {
 
         return !parent.isEmpty();
