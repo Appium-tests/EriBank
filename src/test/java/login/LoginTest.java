dcc3821fe12c03659fbd2d69a949b6fc0820f92e
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-01 09:12:01 +0100

    Added the TestHelper and removed unnecessary method

--- src/test/java/login/LoginTest.java
+++ src/test/java/login/LoginTest.java
@@ -4,39 +4,30 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
+import qa.helpers.TestHelper;
 import qa.pageobject.homeview.HomeView;
-import qa.pageobject.loginview.LoginView;
+import qa.steps.LoginSteps;
 import qa.testdataproviders.TestDataProviders;
 import qa.utils.Credentials;
 
 
 public class LoginTest extends BaseTest {
 
-    private LoginView loginView;
+    private LoginSteps loginSteps;
+    private TestHelper testHelper;
 
     @BeforeMethod
     public void create() {
 
-        loginView = new LoginView(getDriver());
+        loginSteps = new LoginSteps(getDriver());
+        testHelper = new TestHelper();
     }
 
     private void setCredentials(Credentials credentials) {
 
-        loginView.setUsername(credentials.getUsername());
-        loginView.setPassword(credentials.getPassword());
-        loginView.tapLoginButton();
-    }
-
-    private void validateAlertFrame() {
-
-        Assert.assertTrue(loginView.getAlertFrame().isDisplayed(),
-                "The alert frame is not displayed");
-        Assert.assertEquals(loginView.getAlertFrame().getTitle(),
-                "Alert", "Incorrect title");
-        Assert.assertEquals(loginView.getAlertFrame().getMessage(),
-                "Invalid username or password!", "Incorrect message content");
-        Assert.assertTrue(loginView.getAlertFrame().isButtonDisplayed(),
-                "The \"Close\" button is not displayed");
+        loginSteps.setUsername(credentials.getUsername());
+        loginSteps.setPassword(credentials.getPassword());
+        loginSteps.tapLoginButton();
     }
 
     @Test(dataProvider = "CR_correct", dataProviderClass = TestDataProviders.class)
@@ -52,49 +43,47 @@ public class LoginTest extends BaseTest {
     public void correctUsernameWithUppercase(Credentials credentials) {
 
         setCredentials(credentials);
-
-        validateAlertFrame();
-        loginView.getAlertFrame().tapButton();
+        testHelper.checkWhenDataIsIncorrect(loginSteps.getLoginView().getAlertFrame(),
+                credentials.getTitle(), credentials.getMessage());
     }
 
     @Test(dataProvider = "CR_correctPasswordWithUppercase", dataProviderClass = TestDataProviders.class)
     public void correctPasswordWithUppercase(Credentials credentials) {
 
         setCredentials(credentials);
-
-        validateAlertFrame();
-        loginView.getAlertFrame().tapButton();
+        testHelper.checkWhenDataIsIncorrect(loginSteps.getLoginView().getAlertFrame(),
+                credentials.getTitle(), credentials.getMessage());
     }
 
     @Test(dataProvider = "CR_incorrectUsername", dataProviderClass = TestDataProviders.class)
     public void incorrectUsername(Credentials credentials) {
 
         setCredentials(credentials);
-        validateAlertFrame();
-        loginView.getAlertFrame().tapButton();
+        testHelper.checkWhenDataIsIncorrect(loginSteps.getLoginView().getAlertFrame(),
+                credentials.getTitle(), credentials.getMessage());
     }
 
     @Test(dataProvider = "CR_blankUsernameField", dataProviderClass = TestDataProviders.class)
     public void blankUsernameField(Credentials credentials) {
 
         setCredentials(credentials);
-        validateAlertFrame();
-        loginView.getAlertFrame().tapButton();
+        testHelper.checkWhenDataIsIncorrect(loginSteps.getLoginView().getAlertFrame(),
+                credentials.getTitle(), credentials.getMessage());
     }
 
     @Test(dataProvider = "CR_incorrectPassword", dataProviderClass = TestDataProviders.class)
     public void incorrectPassword(Credentials credentials) {
 
         setCredentials(credentials);
-        validateAlertFrame();
-        loginView.getAlertFrame().tapButton();
+        testHelper.checkWhenDataIsIncorrect(loginSteps.getLoginView().getAlertFrame(),
+                credentials.getTitle(), credentials.getMessage());
     }
 
     @Test(dataProvider = "CR_incorrectPassword", dataProviderClass = TestDataProviders.class)
     public void blankPasswordField(Credentials credentials) {
 
         setCredentials(credentials);
-        validateAlertFrame();
-        loginView.getAlertFrame().tapButton();
+        testHelper.checkWhenDataIsIncorrect(loginSteps.getLoginView().getAlertFrame(),
+                credentials.getTitle(), credentials.getMessage());
     }
 }
