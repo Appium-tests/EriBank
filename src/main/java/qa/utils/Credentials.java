9be158e02be16a22dc475fcd82ad07de0d0684d7
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-01 07:33:19 +0100

    Added fields

--- src/main/java/qa/utils/Credentials.java
+++ src/main/java/qa/utils/Credentials.java
@@ -7,9 +7,13 @@ public class Credentials {
 
     private final String username;
     private final String password;
-    public Credentials(String username, String password) {
+    private final String title;
+    private final String message;
+    public Credentials(String username, String password, String title, String message) {
 
         this.username = username;
         this.password = password;
+        this.title = title;
+        this.message = message;
     }
 }
