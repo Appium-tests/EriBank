ca066004910847fea7c68a4c042bb3d566319355
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-01 10:50:52 +0100

    Created the MakePaymentSteps class

---
+++ src/main/java/qa/steps/MakePaymentSteps.java
@@ -0,0 +1,56 @@
+package qa.steps;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.qameta.allure.Step;
+import qa.pageobject.makepaymentview.MakePaymentView;
+
+public class MakePaymentSteps {
+
+    private final MakePaymentView makePaymentView;
+
+    public MakePaymentSteps(AndroidDriver driver) {
+
+        makePaymentView = new MakePaymentView(driver);
+    }
+
+    @Step("Set phone")
+    public void setPhone(String phone) {
+
+        makePaymentView.setPhone(phone);
+    }
+
+    @Step("Set name")
+    public void setName(String name) {
+
+        makePaymentView.setName(name);
+    }
+
+    @Step("Set amount")
+    public void setAmount(String amount) {
+
+        makePaymentView.setAmount(amount);
+    }
+
+    @Step("Set country")
+    public void setCountry(String country) {
+
+        makePaymentView.setCountry(country);
+    }
+
+    @Step("Tap the \"Send payment\" button")
+    public void tapSendPaymentButton() {
+
+        makePaymentView.tapSendPaymentButton();
+    }
+
+    @Step("Tap the \"Cancel\" button")
+    public void tapCancelButton() {
+
+        makePaymentView.tapCancelButton();
+    }
+
+    public MakePaymentView getMakePaymentView() {
+
+        return makePaymentView;
+    }
+}
