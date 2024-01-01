c651d55dcac1ae978980662573da85dd083d0b49
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-01 18:04:12 +0100

    Created the Country class

---
+++ src/main/java/qa/utils/Country.java
@@ -0,0 +1,16 @@
+package qa.utils;
+
+import lombok.Getter;
+
+@Getter
+public class Country {
+
+    private final String name;
+    private final int index;
+
+    public Country(String name, int index) {
+
+        this.name = name;
+        this.index = index;
+    }
+}
