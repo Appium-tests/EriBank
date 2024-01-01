c5b5f1cbc15c26532f2b4dcc80b1c39dc56c7196
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-01 07:30:38 +0100

    Renamed fields

--- src/main/java/qa/utils/Payment.java
+++ src/main/java/qa/utils/Payment.java
@@ -9,16 +9,16 @@ public class Payment {
     private final String name;
     private final String amount;
     private final String country;
-    private final String alertTitle;
-    private final String alertMessage;
+    private final String title;
+    private final String message;
 
-    public Payment(String phone, String name, String amount, String country, String alertTitle, String alertMessage) {
+    public Payment(String phone, String name, String amount, String country, String title, String message) {
 
         this.phone = phone;
         this.name = name;
         this.amount = amount;
         this.country = country;
-        this.alertTitle = alertTitle;
-        this.alertMessage = alertMessage;
+        this.title = title;
+        this.message = message;
     }
 }
