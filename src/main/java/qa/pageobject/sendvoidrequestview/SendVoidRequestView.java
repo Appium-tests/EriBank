c0998f3d8ac50b3b156116e1af7dd283b6fdbe4c
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-02 08:06:07 +0100

    Created the SendVoidRequestView class

---
+++ src/main/java/qa/pageobject/sendvoidrequestview/SendVoidRequestView.java
@@ -0,0 +1,40 @@
+package qa.pageobject.sendvoidrequestview;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
+import qa.base.BaseView;
+
+import java.util.List;
+
+public class SendVoidRequestView extends BaseView {
+
+    public SendVoidRequestView(AndroidDriver driver) {
+
+        super(driver);
+    }
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/recordButton")
+    List<WebElement> recordButton;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/stopButton")
+    List<WebElement> stopButton;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/playButton")
+    List<WebElement> playButton;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/sendButton")
+    List<WebElement> sendButton;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/BackButton")
+    List<WebElement> backButton;
+
+    public boolean isDisplayed() {
+
+        return !recordButton.isEmpty() &&
+               !stopButton.isEmpty() &&
+               !playButton.isEmpty() &&
+               !sendButton.isEmpty() &&
+               !backButton.isEmpty();
+    }
+}
