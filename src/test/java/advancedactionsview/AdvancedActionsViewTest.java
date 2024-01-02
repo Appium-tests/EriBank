9c8a6466c5fe5d004aa8c9952c5604819b9a1a32
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-02 10:57:17 +0100

    Created tests

---
+++ src/test/java/advancedactionsview/AdvancedActionsViewTest.java
@@ -0,0 +1,69 @@
+package advancedactionsview;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Story;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import qa.base.BaseTest;
+import qa.enums.View;
+import qa.enums.WebViewContext;
+import qa.helpers.Authentication;
+import qa.homeviewmanager.HomeViewManager;
+import qa.pageobject.homeview.HomeView;
+import qa.pageobject.sendvoidrequestview.SendVoidRequestView;
+import qa.steps.AdvancedActionsSteps;
+
+import java.net.MalformedURLException;
+
+public class AdvancedActionsViewTest extends BaseTest {
+
+    private AdvancedActionsSteps advancedActionsSteps;
+
+    @BeforeMethod
+    public void create() {
+
+        Authentication.perform(getDriver());
+        HomeViewManager.open(getDriver(), View.ADVANCED_ACTIONS);
+
+        advancedActionsSteps = new AdvancedActionsSteps(getDriver());
+    }
+
+    @Test
+    @Description("Checking whether the \"Send Void Request\" view opens after touching the \"Send Void Request\" button.")
+    @Story("Touching the \"Send Void Request\" button")
+    public void sendVoidRequestButton() {
+
+        advancedActionsSteps.tapSendVoidRequestButton();
+        SendVoidRequestView sendVoidRequestView = new SendVoidRequestView(getDriver());
+
+        Assert.assertTrue(sendVoidRequestView.isDisplayed(),
+                "The \"Send Void Request\" view is not displayed");
+    }
+
+    @Test
+    @Description("Checking whether the \"https://digital.ai...\" website opens after touching the \"EriBank Browser\" button.")
+    @Story("Touching the \"EriBank Browser\" button")
+    public void eriBankBrowserButton() throws MalformedURLException, InterruptedException {
+
+        advancedActionsSteps.tapEriBankBrowserButton();
+
+        String expectedUrl = "https://digital.ai";
+        getDriver().context(WebViewContext.CHROME.getName());
+
+        Assert.assertTrue(getDriver().getCurrentUrl().contains(expectedUrl),
+                "Incorrect url");
+    }
+
+    @Test
+    @Description("Verify that the \"Advanced Actions\" view is closed after touching the \"Back\" button")
+    @Story("Touching the \"Back\" button")
+    public void closingView() {
+
+        advancedActionsSteps.tapBackButton();
+        HomeView homeView = new HomeView(getDriver());
+
+        Assert.assertTrue(homeView.isDisplayed(),
+                "The \"Advanced Actions\" view is not closed");
+    }
+}
