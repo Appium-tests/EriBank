c49e5a403ce16f755ba5c627228ae6d04588bed1
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-07 11:55:17 +0100

    Created the EnvironmentInfo class

---
+++ src/main/java/qa/environmentinfo/EnvironmentInfo.java
@@ -0,0 +1,19 @@
+package qa.environmentinfo;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EnvironmentInfo {
+
+    private static Logger logger = LoggerFactory.getLogger(EnvironmentInfo.class);
+    public static void logInfo(AndroidDriver driver) {
+
+        logger.info("Device name: " + driver.getCapabilities().getCapability("deviceName"));
+        logger.info("Device time: " + driver.getCapabilities().getCapability("deviceTime"));
+        logger.info("Battery state: " + driver.getBatteryInfo().getState().name());
+        logger.info("Battery level: " + driver.getBatteryInfo().getLevel());
+        logger.info("Platform name: " + driver.getCapabilities().getCapability("platformName"));
+        logger.info("Platform version: " + driver.getCapabilities().getCapability("platformVersion"));
+    }
+}
