11b1762a14ed3c06c69057b7867dc23bb645bba1
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-08 21:39:53 +0100

    Added static final fields

--- src/main/java/qa/driver/Driver.java
+++ src/main/java/qa/driver/Driver.java
@@ -7,20 +7,24 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.MalformedURLException;
 import java.net.URI;
 
-
 public class Driver {
-
+    private static final String AUTOMATION_NAME = "uiautomator2";
+    private static final String UDID = "UDID";
+    private static final String PLATFORM_NAME = "Android";
+    private static final String PLATFORM_VERSION = "11";
+    private static final String APP_PACKAGE = "com.experitest.ExperiBank";
+    private static final String APP_ACTIVITY = ".LoginActivity";
     private static final URI uri = URI.create("http://127.0.0.1:4723/");
 
     private static DesiredCapabilities getCapabilities() {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("automationName", "uiautomator2");
-        capabilities.setCapability(MobileCapabilityType.UDID, "UDID");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13");
-        capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.ExperiBank");
-        capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".LoginActivity");
+        capabilities.setCapability("automationName", AUTOMATION_NAME);
+        capabilities.setCapability(MobileCapabilityType.UDID, UDID);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, PLATFORM_NAME);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, PLATFORM_VERSION);
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, APP_PACKAGE);
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, APP_ACTIVITY);
         capabilities.setCapability("appium:autoGrantPermission", true);
 
         return capabilities;
