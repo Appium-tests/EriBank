8cd4110b80271eb5e4d16d440cf8c0f623ee128a
pawelaksman <pablo_xmen@op.pl>
2024-03-12 00:28:42 +0100

    Updated

--- src/main/java/qa/helpers/Authentication.java
+++ src/main/java/qa/helpers/Authentication.java
@@ -1,19 +1,20 @@
 package qa.helpers;
 
 import io.appium.java_client.android.AndroidDriver;
-import qa.json.JSONReader;
+import qa.dataproviders.CredentialsDataProviders;
 import qa.pageobject.loginview.LoginView;
-import qa.utils.Credentials;
+import qa.models.Credentials;
 
 public class Authentication {
 
     public static void perform(AndroidDriver driver) {
 
-        Credentials[] credentials = JSONReader.getCredentials("correct");
+        CredentialsDataProviders dataProviders = new CredentialsDataProviders();
+        Credentials[] credentials = (Credentials[]) dataProviders.correct();
 
         LoginView loginView = new LoginView(driver);
         loginView.setUsername(credentials[0].getUsername());
         loginView.setPassword(credentials[0].getPassword());
-        loginView.tapLoginButton();
+        loginView.touchLoginButton();
     }
 }
