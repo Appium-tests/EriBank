80fede4975ac57ab8f7e2180de260c212edd2747
pawelaksman <pablo_xmen@op.pl>
2024-03-11 23:58:55 +0100

    Removed unnecessary annotations

--- src/test/java/login/LoginTest.java
+++ src/test/java/login/LoginTest.java
@@ -3,107 +3,111 @@ package login;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
+import io.qase.api.annotation.QaseId;
+import io.qase.api.annotation.QaseTitle;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
-import qa.helpers.TestHelper;
 import qa.pageobject.homeview.HomeView;
-import qa.steps.LoginSteps;
-import qa.testdataproviders.TestDataProviders;
-import qa.utils.Credentials;
+import qa.pageobject.loginview.LoginView;
+import qa.dataproviders.CredentialsDataProviders;
+import qa.models.Credentials;
+import qa.utils.DataProviderNames;
 
 @Epic("E2E")
 @Feature("Login")
 public class LoginTest extends BaseTest {
 
-    private LoginSteps loginSteps;
-    private TestHelper testHelper;
+    private LoginView loginView;
+    private HomeView homeView;
 
     @BeforeMethod
     public void create() {
 
-        loginSteps = new LoginSteps(getDriver());
-        testHelper = new TestHelper();
+        loginView = new LoginView(getDriver());
+        homeView = new HomeView(getDriver());
     }
 
-    private void setCredentials(Credentials credentials) {
+    private void actions(Credentials credentials) {
 
-        loginSteps.setUsername(credentials.getUsername());
-        loginSteps.setPassword(credentials.getPassword());
-        loginSteps.tapLoginButton();
+        loginView.setUsername(credentials.getUsername());
+        loginView.setPassword(credentials.getPassword());
+        loginView.touchLoginButton();
     }
 
-    @Test(dataProvider = "CR_correct", dataProviderClass = TestDataProviders.class)
-    @Description("System verification when logging in using correct credentials")
-    @Story("Logging in with correct credentials")
+    @Test(dataProvider = DataProviderNames.CORRECT, dataProviderClass = CredentialsDataProviders.class)
+    @QaseId(5)
+    @QaseTitle("Login with correct credentials")
+    @Description("Login with correct credentials")
     public void correctCredentials(Credentials credentials) {
 
-        setCredentials(credentials);
-        HomeView homeView = new HomeView(getDriver());
-
+        actions(credentials);
         Assert.assertTrue(homeView.isDisplayed(),
-                "The user is not logged in");
+                "A user is not logged in");
     }
 
-    @Test(dataProvider = "CR_correctUsernameWithUppercase", dataProviderClass = TestDataProviders.class)
-    @Description("System verification when logging in using a correct username with lowercase and uppercase letters")
-    @Story("Logging in with correct username with lowercase and uppercase letters")
-    public void correctUsernameWithUppercase(Credentials credentials) {
 
-        setCredentials(credentials);
-        testHelper.checkWhenDataIsIncorrect(loginSteps.getLoginView().getAlertFrame(),
-                credentials.getTitle(), credentials.getMessage());
+    @Test(dataProvider = DataProviderNames.USERNAME_WITH_UPPER_LETTERS, dataProviderClass = CredentialsDataProviders.class)
+    @QaseId(6)
+    @QaseTitle("Correct username with upper letters")
+    @Description("Correct username with upper letters")
+    public void correctUsernameWithUpperLetters(Credentials credentials) {
+
+        actions(credentials);
+        Assert.assertFalse(loginView.getALERT_FRAME().isDisplayed(), "The alert is displayed");
+        Assert.assertTrue(homeView.isDisplayed(), "The home view is not opened");
     }
 
-    @Test(dataProvider = "CR_correctPasswordWithUppercase", dataProviderClass = TestDataProviders.class)
-    @Description("System verification when logging in using a valid password with lowercase and uppercase letters")
-    @Story("Logging in with correct password with lowercase and uppercase letters")
-    public void correctPasswordWithUppercase(Credentials credentials) {
+    @Test(dataProvider = DataProviderNames.PASSWORD_WITH_UPPER_LETTERS, dataProviderClass = CredentialsDataProviders.class)
+    @QaseId(7)
+    @QaseTitle("Correct password with upper letters")
+    @Description("Correct password with upper letters")
+    public void correctPasswordWithUpperLetters(Credentials credentials) {
 
-        setCredentials(credentials);
-        testHelper.checkWhenDataIsIncorrect(loginSteps.getLoginView().getAlertFrame(),
-                credentials.getTitle(), credentials.getMessage());
+        actions(credentials);
+        Assert.assertTrue(loginView.getALERT_FRAME().isDisplayed(), "The alert is not displayed");
     }
 
-    @Test(dataProvider = "CR_incorrectUsername", dataProviderClass = TestDataProviders.class)
-    @Description("System verification when logging in using a valid username with incorrect username")
-    @Story("Logging in with incorrect username")
+    @Test(dataProvider = DataProviderNames.INCORRECT_USERNAME, dataProviderClass = CredentialsDataProviders.class)
+    @QaseId(8)
+    @QaseTitle("Incorrect username")
+    @Description("Incorrect username")
     public void incorrectUsername(Credentials credentials) {
 
-        setCredentials(credentials);
-        testHelper.checkWhenDataIsIncorrect(loginSteps.getLoginView().getAlertFrame(),
-                credentials.getTitle(), credentials.getMessage());
+        actions(credentials);
+        Assert.assertTrue(loginView.getALERT_FRAME().isDisplayed(), "The alert is not displayed");
     }
 
-    @Test(dataProvider = "CR_blankUsernameField", dataProviderClass = TestDataProviders.class)
-    @Description("System verification when logging in using a valid username with blank username field")
-    @Story("Logging in with blank username field")
+    @Test(dataProvider = DataProviderNames.BLANK_USERNAME_FIELD, dataProviderClass = CredentialsDataProviders.class)
+    @QaseId(9)
+    @QaseTitle("Blank the \"Username\" field")
+    @Description("Blank the \"Username\" field")
     public void blankUsernameField(Credentials credentials) {
 
-        setCredentials(credentials);
-        testHelper.checkWhenDataIsIncorrect(loginSteps.getLoginView().getAlertFrame(),
-                credentials.getTitle(), credentials.getMessage());
+        loginView.setPassword(credentials.getPassword());
+        loginView.touchLoginButton();
+        Assert.assertTrue(loginView.getALERT_FRAME().isDisplayed(), "The alert is not displayed");
     }
 
-    @Test(dataProvider = "CR_incorrectPassword", dataProviderClass = TestDataProviders.class)
-    @Description("System verification when logging in using a valid username with incorrect password")
-    @Story("Logging in with incorrect password")
+    @Test(dataProvider = DataProviderNames.INCORRECT_PASSWORD, dataProviderClass = CredentialsDataProviders.class)
+    @QaseId(10)
+    @QaseTitle("Incorrect password")
+    @Description("Incorrect password")
     public void incorrectPassword(Credentials credentials) {
 
-        setCredentials(credentials);
-        testHelper.checkWhenDataIsIncorrect(loginSteps.getLoginView().getAlertFrame(),
-                credentials.getTitle(), credentials.getMessage());
+        actions(credentials);
+        Assert.assertTrue(loginView.getALERT_FRAME().isDisplayed(), "The alert is not displayed");
     }
 
-    @Test(dataProvider = "CR_incorrectPassword", dataProviderClass = TestDataProviders.class)
-    @Description("System verification when logging in using a valid username with blank password field")
-    @Story("Logging in with blank password field")
+    @Test(dataProvider = DataProviderNames.BLANK_PASSWORD_FIELD, dataProviderClass = CredentialsDataProviders.class)
+    @QaseId(11)
+    @QaseTitle("Blank the \"Password\" field")
+    @Description("Blank the \"Password\" field")
     public void blankPasswordField(Credentials credentials) {
 
-        setCredentials(credentials);
-        testHelper.checkWhenDataIsIncorrect(loginSteps.getLoginView().getAlertFrame(),
-                credentials.getTitle(), credentials.getMessage());
+        loginView.setUsername(credentials.getUsername());
+        loginView.touchLoginButton();
+        Assert.assertTrue(loginView.getALERT_FRAME().isDisplayed(), "The alert is not displayed");
     }
 }
