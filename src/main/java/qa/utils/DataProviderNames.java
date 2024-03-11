c2d4e7096653ba7148ae8c5d8d43248a97b07da8
pawelaksman <pablo_xmen@op.pl>
2024-03-11 23:56:16 +0100

    Created

---
+++ src/main/java/qa/utils/DataProviderNames.java
@@ -0,0 +1,20 @@
+package qa.utils;
+
+public class DataProviderNames {
+
+    public static final String CORRECT = "correct";
+    public static final String USERNAME_WITH_UPPER_LETTERS = "usernameWithUpperLetters";
+    public static final String PASSWORD_WITH_UPPER_LETTERS = "passwordWithUpperLetters";
+    public static final String INCORRECT_USERNAME = "incorrectUsername";
+    public static final String BLANK_USERNAME_FIELD = "blankUsernameField";
+    public static final String INCORRECT_PASSWORD = "incorrectPassword";
+    public static final String BLANK_PASSWORD_FIELD = "blankPasswordField";
+    public static final String INCORRECT_PHONE = "incorrectPhone";
+    public static final String BLANK_PHONE_FIELD = "blankPhoneField";
+    public static final String INCORRECT_NAME = "incorrectName";
+    public static final String BLANK_NAME_FIELD = "blankNameField";
+    public static final String INCORRECT_AMOUNT = "incorrectAmount";
+    public static final String BLANK_AMOUNT_FIELD = "blankAmountField";
+    public static final String INCORRECT_COUNTRY = "incorrectCountry";
+    public static final String BLANK_COUNTRY_FIELD = "blankCountryField";
+}
