dec1f6bd270c20e41ea54eeafc3c861e048cc15d
pawelaksman <pablo_xmen@op.pl>
2024-03-13 23:26:00 +0100

    Added the @Getter

--- src/main/java/qa/base/BaseView.java
+++ src/main/java/qa/base/BaseView.java
@@ -2,12 +2,17 @@ package qa.base;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import lombok.Getter;
 import org.openqa.selenium.support.PageFactory;
 
+@Getter
 public class BaseView {
+
+    private final AndroidDriver DRIVER;
 
     public BaseView(AndroidDriver driver) {
 
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+        this.DRIVER = driver;
     }
 }
