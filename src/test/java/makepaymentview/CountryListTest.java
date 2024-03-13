b359f04a97321cfbdae576df6446c68d52574932
pawelaksman <pablo_xmen@op.pl>
2024-03-14 00:07:38 +0100

    Added the method

--- src/test/java/makepaymentview/CountryListTest.java
+++ src/test/java/makepaymentview/CountryListTest.java
@@ -4,15 +4,19 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import io.qase.api.annotation.QaseId;
+import io.qase.api.annotation.QaseTitle;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
 import qa.helpers.Authentication;
+import qa.pageobject.countrylist.CountryList;
 import qa.pageobject.homeview.HomeView;
+import qa.pageobject.makepaymentview.MakePaymentView;
 import qa.steps.CountryListSteps;
 import qa.steps.MakePaymentSteps;
-import qa.testdataproviders.TestDataProviders;
+import qa.dataproviders.TestDataProviders;
 import qa.utils.Country;
 
 @Epic("E2E")
@@ -20,32 +24,48 @@ import qa.utils.Country;
 public class CountryListTest extends BaseTest {
 
     private CountryListSteps countryListSteps;
+    private CountryList countryList;
 
     @BeforeMethod
     public void create() {
 
         Authentication.perform(getDriver());
         HomeView homeView = new HomeView(getDriver());
-        homeView.tapMakePaymentButton();
+        homeView.touchMakePaymentButton();
 
         countryListSteps = new CountryListSteps(getDriver());
+        countryList = new CountryList(getDriver());
     }
 
     @Test
-    @Description("Checking whether the country list is displayed after touching the \"Select\" button")
-    @Story("Touching the \"Select\" button")
-    public void selectButton() {
+    public void swiping() {
 
         MakePaymentSteps makePaymentSteps = new MakePaymentSteps(getDriver());
         makePaymentSteps.tapSelectButton();
+
+        countryList.swipeDown();
+        Assert.assertTrue(countryList.isLastItemDisplayed(), "Unable to swipe down the list");
+        countryList.swipeUp();
+        Assert.assertTrue(countryList.isFirstItemDisplayed(), "Unable to swipe up the list");
+    }
+
+    @Test
+    @QaseId(17)
+    @QaseTitle("The \"Select\" button")
+    @Description("The \"Select\" button")
+    public void selectButton() {
+
+        MakePaymentView makePaymentView = new MakePaymentView(getDriver());
+        makePaymentView.touchSelectButton();
 
         Assert.assertTrue(countryListSteps.getCountryList().isDisplayed(),
                 "The country list is not displayed");
     }
 
     @Test(dataProvider = "countries", dataProviderClass = TestDataProviders.class)
-    @Description("Checking whether the country name in the country field is correct after selecting a country from the list")
-    @Story("Selecting a country from the list")
+    @QaseId(18)
+    @QaseTitle("Selecting a country")
+    @Description("Selecting a country")
     public void selectingCountry(Country country) {
 
         MakePaymentSteps makePaymentSteps = new MakePaymentSteps(getDriver());
