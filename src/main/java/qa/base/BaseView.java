d7ed246f76e946f767daba8c9df861026a253f2e
pawelaksman <pablo_xmen@op.pl>
2024-03-18 21:37:53 +0100

    Added the WebDriverWait

--- src/main/java/qa/base/BaseView.java
+++ src/main/java/qa/base/BaseView.java
@@ -4,15 +4,20 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import lombok.Getter;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 @Getter
 public class BaseView {
 
     private final AndroidDriver DRIVER;
+    private final WebDriverWait WEB_DRIVER_WAIT;
 
     public BaseView(AndroidDriver driver) {
 
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
         this.DRIVER = driver;
+        WEB_DRIVER_WAIT = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 }
