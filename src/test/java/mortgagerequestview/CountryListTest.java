ca6aae16cac69783dfc6e495d825a8fba10a16b9
pawelaksman <pablo_xmen@op.pl>
2024-04-09 22:58:23 +0200

    Added annotations

--- src/test/java/mortgagerequestview/CountryListTest.java
+++ src/test/java/mortgagerequestview/CountryListTest.java
@@ -2,6 +2,8 @@ package mortgagerequestview;
 
 import base.CountryListBaseTest;
 import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.testng.Tag;
 import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
 import org.testng.Assert;
@@ -33,9 +35,11 @@ public class CountryListTest extends CountryListBaseTest {
     }
 
     @Test(priority = 1, groups = "closed")
+    @Tag("List")
+    @Owner("Paweł Aksman")
+    @Description("The \"Select\" button")
     @QaseId(31)
     @QaseTitle("The \"Select\" button")
-    @Description("The \"Select\" button")
     public void selectButton() {
 
         mortgageRequestView.touchSelectButton();
@@ -43,18 +47,22 @@ public class CountryListTest extends CountryListBaseTest {
     }
 
     @Test(priority = 2, groups = "opened")
+    @Tag("List")
+    @Owner("Paweł Aksman")
+    @Description("Swiping the list")
     @QaseId(32)
     @QaseTitle("Swiping the list")
-    @Description("Swiping the list")
     public void swiping() {
 
         baseSwiping();
     }
 
     @Test(priority = 3, groups = "opened", dataProvider = DataProviderNames.RANDOM_INDEXES, dataProviderClass = RandomIndexesDataProvider.class)
+    @Tag("List")
+    @Owner("Paweł Aksman")
+    @Description("Selecting a country")
     @QaseId(33)
     @QaseTitle("Selecting a country")
-    @Description("Selecting a country")
     public void selectingCountry(int index) {
 
         baseSelectingCountry(index);
