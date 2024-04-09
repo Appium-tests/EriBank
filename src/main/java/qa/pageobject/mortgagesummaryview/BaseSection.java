2e61f90bde086cad7e642c54f4330a6790d60c77
pawelaksman <pablo_xmen@op.pl>
2024-04-09 18:42:32 +0200

    Created

---
+++ src/main/java/qa/pageobject/mortgagesummaryview/BaseSection.java
@@ -0,0 +1,61 @@
+package qa.pageobject.mortgagesummaryview;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import qa.pageobject.base.BaseView;
+
+public class BaseSection extends BaseView {
+
+    private final By parent;
+    private final String itemXPath;
+
+    public BaseSection(AndroidDriver driver, String parentXPath, String itemXPath) {
+
+        super(driver);
+
+        parent = By.xpath(parentXPath);
+        this.itemXPath = itemXPath;
+    }
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/saveButton")
+    WebElement saveButton;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/backButton")
+    WebElement backButton;
+
+    @io.qameta.allure.Step("Touch the item")
+    @io.qase.api.annotation.Step("Touch the item")
+    public void touchItem(String name) {
+
+        getWEB_DRIVER_WAIT().until(ExpectedConditions.elementToBeClickable(
+                getDRIVER().findElement(By.xpath(itemXPath + name + "']"))
+        )).click();
+    }
+
+    public String getItemCheckedStatus(String name) {
+
+        return getDRIVER().findElement(By.xpath(itemXPath + name + "']")).getAttribute("checked");
+    }
+
+    public WebElement getParent() {
+
+        return getDRIVER().findElement(parent);
+    }
+
+    @io.qameta.allure.Step("Touch the \"Save\" button")
+    @io.qase.api.annotation.Step("Touch the \"Save\" button")
+    public void touchSaveButton() {
+
+        getWEB_DRIVER_WAIT().until(ExpectedConditions.elementToBeClickable(saveButton)).click();
+    }
+
+    @io.qameta.allure.Step("Touch the \"Back\" button")
+    @io.qase.api.annotation.Step("Touch the \"Back\" button")
+    public void touchBackButton() {
+
+        getWEB_DRIVER_WAIT().until(ExpectedConditions.elementToBeClickable(backButton)).click();
+    }
+}
