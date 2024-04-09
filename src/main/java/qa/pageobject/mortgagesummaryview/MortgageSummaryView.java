56004a07cd554d284bdbdaf2f4626a462084f09f
pawelaksman <pablo_xmen@op.pl>
2024-04-09 18:44:15 +0200

    Created

---
+++ src/main/java/qa/pageobject/mortgagesummaryview/MortgageSummaryView.java
@@ -0,0 +1,87 @@
+package qa.pageobject.mortgagesummaryview;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import lombok.Getter;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import qa.pageobject.base.BaseView;
+import qa.support.SwipingHelper;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MortgageSummaryView extends BaseView {
+
+    @Getter
+    private final TypeOfLoanSection typeOfLoanSection;
+    @Getter
+    private final NumberOfYearsView numberOfYearsView;
+    @Getter
+    private final TypeOfOccupationSection typeOfOccupationSection;
+    @Getter
+    private final YearlyIncomeSection yearlyIncomeSection;
+
+    public MortgageSummaryView(AndroidDriver driver) {
+
+        super(driver);
+
+        this.typeOfLoanSection = new TypeOfLoanSection(getDRIVER());
+        this.numberOfYearsView = new NumberOfYearsView(getDRIVER());
+        this.typeOfOccupationSection = new TypeOfOccupationSection(getDRIVER());
+        this.yearlyIncomeSection = new YearlyIncomeSection(getDRIVER());
+    }
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/typeOfLoanListView")
+    List<WebElement> typeOfLoanListView;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/numberOfYearsListView")
+    List<WebElement> numberOfYearsListView;
+
+    @AndroidFindBy(id = "com.experitest.ExperiBank:id/typeOfOccupationListView")
+    List<WebElement> typeOfOccupationListView;
+
+    @AndroidFindBy(className = "android.widget.CheckedTextView")
+    List<WebElement> itemsList;
+
+    public boolean isDisplayed() {
+
+        return !typeOfLoanListView.isEmpty() &&
+               !numberOfYearsListView.isEmpty() &&
+               !typeOfOccupationListView.isEmpty();
+    }
+
+    public int getSize() {
+
+        return itemsList.size();
+    }
+
+    public void swipeDown() {
+
+        Point to = itemsList.get(0).getLocation();
+        Point from = itemsList.get(itemsList.size() - 1).getLocation();
+        SwipingHelper.swipe(from, to, getDRIVER());
+    }
+
+    public void swipeUp() {
+
+        Optional<WebElement> from = itemsList.stream().filter(WebElement::isDisplayed).findFirst();
+        Point to = itemsList.get(itemsList.size() - 1).getLocation();
+        from.ifPresent(webElement -> SwipingHelper.swipe(webElement.getLocation(), to, getDRIVER()));
+    }
+
+    public String getText() {
+
+        return itemsList.get(itemsList.size() - 1).getText();
+    }
+
+    public boolean isFirstItemVisible() {
+
+        return itemsList.get(0).getText().equals("Home");
+    }
+
+    public boolean isLastItemVisible() {
+
+        return itemsList.get(itemsList.size() - 1).getText().equals("More");
+    }
+}
