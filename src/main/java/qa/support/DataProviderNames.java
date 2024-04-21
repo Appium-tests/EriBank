13436d65429cc7486c0e5ed5a9ac82690a89d295
pawelaksman <pablo_xmen@op.pl>
2024-04-22 00:35:15 +0200

    Added the field

--- src/main/java/qa/support/DataProviderNames.java
+++ src/main/java/qa/support/DataProviderNames.java
@@ -2,29 +2,31 @@ package qa.support;
 
 public class DataProviderNames {
 
-    public static final String CORRECT = "correct";
-    public static final String USERNAME_WITH_UPPER_LETTERS = "usernameWithUpperLetters";
-    public static final String PASSWORD_WITH_UPPER_LETTERS = "passwordWithUpperLetters";
-    public static final String INCORRECT_USERNAME = "incorrectUsername";
-    public static final String BLANK_USERNAME_FIELD = "blankUsernameField";
-    public static final String INCORRECT_PASSWORD = "incorrectPassword";
-    public static final String BLANK_PASSWORD_FIELD = "blankPasswordField";
-    public static final String INCORRECT_PHONE = "incorrectPhone";
-    public static final String BLANK_PHONE_FIELD = "blankPhoneField";
-    public static final String INCORRECT_NAME = "incorrectName";
-    public static final String BLANK_NAME_FIELD = "blankNameField";
-    public static final String INCORRECT_AMOUNT = "incorrectAmount";
     public static final String BLANK_AMOUNT_FIELD = "blankAmountField";
-    public static final String INCORRECT_COUNTRY = "incorrectCountry";
     public static final String BLANK_COUNTRY_FIELD = "blankCountryField";
-    public static final String INCORRECT_FIRST_NAME = "incorrectFirstName";
-    public static final String INCORRECT_LAST_NAME = "incorrectLastName";
-    public static final String INCORRECT_AGE = "incorrectAge";
+    public static final String BLANK_NAME_FIELD = "blankNameField";
+    public static final String BLANK_PASSWORD_FIELD = "blankPasswordField";
+    public static final String BLANK_PHONE_FIELD = "blankPhoneField";
+    public static final String BLANK_USERNAME_FIELD = "blankUsernameField";
+    public static final String CORRECT = "correct";
+    public static final String COUNTRIES = "country";
     public static final String INCORRECT_ADDRESS_1 = "incorrectAddress1";
     public static final String INCORRECT_ADDRESS_2 = "incorrectAddress2";
-    public static final String TYPE_OF_LOAN = "typeOfLoan";
+    public static final String INCORRECT_AGE = "incorrectAge";
+    public static final String INCORRECT_AMOUNT = "incorrectAmount";
+    public static final String INCORRECT_COUNTRY = "incorrectCountry";
+    public static final String INCORRECT_FIRST_NAME = "incorrectFirstName";
+    public static final String INCORRECT_LAST_NAME = "incorrectLastName";
+    public static final String INCORRECT_NAME = "incorrectName";
+    public static final String INCORRECT_PASSWORD = "incorrectPassword";
+    public static final String INCORRECT_PHONE = "incorrectPhone";
+    public static final String INCORRECT_USERNAME = "incorrectUsername";
     public static final String NUMBER_OF_YEARS = "numberOfYears";
+    public static final String PASSWORD_WITH_UPPER_LETTERS = "passwordWithUpperLetters";
+    public static final String SCROLLABLE_COUNTRIES = "scrollableCountries";
+    public static final String TYPE_OF_LOAN = "typeOfLoan";
     public static final String TYPE_OF_OCCUPATION = "typeOfOccupation";
+    public static final String USERNAME_WITH_UPPER_LETTERS = "usernameWithUpperLetters";
     public static final String YEARLY_INCOME = "yearlyIncome";
-    public static final String COUNTRIES = "country";
 }
+
