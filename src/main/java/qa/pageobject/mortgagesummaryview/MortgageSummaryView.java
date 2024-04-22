1e3c6eedea50d0b4dfe2f9f11e3f8a91335f2613
pawelaksman <pablo_xmen@op.pl>
2024-04-22 23:57:28 +0200

    Removed methods

--- src/main/java/qa/pageobject/mortgagesummaryview/MortgageSummaryView.java
+++ src/main/java/qa/pageobject/mortgagesummaryview/MortgageSummaryView.java
@@ -3,14 +3,9 @@ package qa.pageobject.mortgagesummaryview;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import lombok.Getter;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import qa.pageobject.base.BaseView;
-import qa.support.SwipingHelper;
-
-import java.util.List;
-import java.util.Optional;
 
 public class MortgageSummaryView extends BaseView {
 
@@ -33,9 +28,6 @@ public class MortgageSummaryView extends BaseView {
         this.yearlyIncomeSection = new YearlyIncomeSection(getDriver());
     }
 
-    @AndroidFindBy(className = "android.widget.CheckedTextView")
-    List<WebElement> itemsList;
-
     @AndroidFindBy(id = "com.experitest.ExperiBank:id/saveButton")
     WebElement saveButton;
 
@@ -46,30 +38,6 @@ public class MortgageSummaryView extends BaseView {
     public void waitForTypeOfLoanSectionListView() {
 
         getWebDriverWait().until(ExpectedConditions.presenceOfElementLocated(typeOfLoanSection.getListView()));
-    }
-
-    public void swipeDown() {
-
-        Point to = itemsList.get(0).getLocation();
-        Point from = itemsList.get(itemsList.size() - 1).getLocation();
-        SwipingHelper.swipe(from, to, getDriver());
-    }
-
-    public void swipeUp() {
-
-        Optional<WebElement> from = itemsList.stream().filter(WebElement::isDisplayed).findFirst();
-        Point to = itemsList.get(itemsList.size() - 1).getLocation();
-        from.ifPresent(webElement -> SwipingHelper.swipe(webElement.getLocation(), to, getDriver()));
-    }
-
-    public boolean isFirstItemVisible() {
-
-        return itemsList.get(0).getText().equals("Home");
-    }
-
-    public boolean isLastItemVisible() {
-
-        return itemsList.get(itemsList.size() - 1).getText().equals("More");
     }
 
     @io.qameta.allure.Step("Click the \"Save\" button")
