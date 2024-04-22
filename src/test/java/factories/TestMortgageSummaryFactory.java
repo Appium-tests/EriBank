13584e8b20717d0c967508ec09e3f3abbe312d84
pawelaksman <pablo_xmen@op.pl>
2024-04-22 23:58:59 +0200

    Created

---
+++ src/test/java/factories/TestMortgageSummaryFactory.java
@@ -0,0 +1,21 @@
+package factories;
+
+import mortgagesummaryview.ButtonsTest;
+import org.testng.annotations.Factory;
+import qa.dataproviders.SectionsDataProvider;
+import qa.support.DataProviderNames;
+import qa.support.FileReader;
+
+public class TestMortgageSummaryFactory {
+
+    public TestMortgageSummaryFactory() {
+
+        FileReader.load("EB_Sections.json");
+    }
+
+    @Factory(dataProvider = DataProviderNames.YEARLY_INCOME, dataProviderClass = SectionsDataProvider.class, indices = { 2 })
+    public Object[] createInstance(String itemName) {
+
+        return new Object[] {new ButtonsTest(itemName)};
+    }
+}
