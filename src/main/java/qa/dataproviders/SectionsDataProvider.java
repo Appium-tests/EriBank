4a46963942b582394a1567eff96adf07d8fa616c
pawelaksman <pablo_xmen@op.pl>
2024-04-22 11:18:19 +0200

    Added arguments

--- src/main/java/qa/dataproviders/SectionsDataProvider.java
+++ src/main/java/qa/dataproviders/SectionsDataProvider.java
@@ -9,24 +9,24 @@ public class SectionsDataProvider {
     @DataProvider(name = DataProviderNames.TYPE_OF_LOAN)
     public Object[] typeOfLoan() {
 
-        return TestDataLoader.getStringArray(DataProviderNames.TYPE_OF_LOAN);
+        return TestDataLoader.getStringArray(DataProviderNames.TYPE_OF_LOAN, 0);
     }
 
     @DataProvider(name = DataProviderNames.NUMBER_OF_YEARS)
     public Object[] numberOfYears() {
 
-        return TestDataLoader.getStringArray(DataProviderNames.NUMBER_OF_YEARS);
+        return TestDataLoader.getStringArray(DataProviderNames.NUMBER_OF_YEARS, 0);
     }
 
     @DataProvider(name = DataProviderNames.TYPE_OF_OCCUPATION)
     public Object[] typeOfOccupation() {
 
-        return TestDataLoader.getStringArray(DataProviderNames.TYPE_OF_OCCUPATION);
+        return TestDataLoader.getStringArray(DataProviderNames.TYPE_OF_OCCUPATION, 0);
     }
 
     @DataProvider(name = DataProviderNames.YEARLY_INCOME)
     public Object[] yearlyIncome() {
 
-        return TestDataLoader.getStringArray(DataProviderNames.YEARLY_INCOME);
+        return TestDataLoader.getStringArray(DataProviderNames.YEARLY_INCOME, 0);
     }
 }
