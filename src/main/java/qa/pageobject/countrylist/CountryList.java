cee9fb9969ceaed03d9d20a5c5df5d7d3053f155
pawelaksman <pablo_xmen@op.pl>
2024-04-23 22:39:19 +0200

    Removed the method

--- src/main/java/qa/pageobject/countrylist/CountryList.java
+++ src/main/java/qa/pageobject/countrylist/CountryList.java
@@ -1,6 +1,5 @@
 package qa.pageobject.countrylist;
 
-import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import lombok.Getter;
@@ -15,7 +14,6 @@ public class CountryList extends BaseView {
 
     @Getter
     private String countryName = "";
-    private final String partialUiAutomatorString = "new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"";
     private final String partialXPath = "//android.widget.TextView[@resource-id='com.experitest.ExperiBank:id/rowTextView' and @text='";
 
 
@@ -27,14 +25,6 @@ public class CountryList extends BaseView {
     @AndroidFindBy(id = "com.experitest.ExperiBank:id/countryList")
     List<WebElement> parent;
 
-
-    @io.qameta.allure.Step("Swipe to desired element")
-    @io.qase.api.annotation.Step("Swipe to desired element")
-    public void swipeToDesiredItem(String countryName) {
-
-        String uiAutomatorString = partialUiAutomatorString + countryName + "\"))";
-        getDriver().findElement(new AppiumBy.ByAndroidUIAutomator(uiAutomatorString));
-    }
 
     @io.qameta.allure.Step("Select a country")
     @io.qase.api.annotation.Step("Select a country")
