201b4f6d1c461e57827d888e29acb9f4212434b5
pawelaksman <pablo_xmen@op.pl>
2024-04-25 00:30:48 +0200

    Added the method

--- src/test/java/expensereportview/ExpenseReportViewTest.java
+++ src/test/java/expensereportview/ExpenseReportViewTest.java
@@ -8,76 +8,83 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import base.BaseTest;
 import qa.enums.View;
-import qa.support.Authentication;
-import qa.support.DynamicList;
-import qa.support.HomeViewManager;
+import qa.pageobject.expensereportview.ExpenseReportView;
 import qa.pageobject.homeview.HomeView;
-import qa.steps.ExpenseReportSteps;
+import qa.support.Authentication;
+import qa.support.HomeViewManager;
+import qa.support.RandomValue;
 
 @Epic("E2E")
 @Feature("Expense Report functionalities")
 public class ExpenseReportViewTest extends BaseTest {
 
-    private ExpenseReportSteps expenseReportSteps;
+    private ExpenseReportView expenseReportView;
+    private int maxElementsToAdd;
+    private int maxElementsToRemove;
 
-    @BeforeMethod
-    public void create() throws JsonProcessingException {
+
+    @BeforeMethod(onlyForGroups = "withoutAddedElements")
+    public void prepare() throws JsonProcessingException {
+
+        initialize();
+        maxElementsToAdd = RandomValue.get(2, 5);
+    }
+
+    @BeforeMethod(onlyForGroups = {"withAddedElements"})
+    public void prepareWithAddedElements() throws JsonProcessingException {
+
+        initialize();
+        maxElementsToAdd = RandomValue.get(2, 5);
+        addRows();
+        maxElementsToRemove = RandomValue.get(1, expenseReportView.getCount() - 2);
+    }
+
+    private void initialize() throws JsonProcessingException {
 
         Authentication.perform(getDriver());
         HomeViewManager.open(getDriver(), View.EXPENSE_REPORT);
-
-        expenseReportSteps = new ExpenseReportSteps(getDriver());
+        expenseReportView = new ExpenseReportView(getDriver());
     }
 
-    @Test
-    @Tag("View")
-    @Owner("Paweł Aksman")
-    @Description("Checking whether a row is added after touching the \"Add\" button")
-    @Story("Touching the \"Add\" button")
-    public void addingRow() {
+    private void addRows() {
 
-        int currentCount = expenseReportSteps.getExpenseReportView().getCount();
-        int expectedCount = currentCount + 1;
-
-        expenseReportSteps.tapAddButton();
-
-        Assert.assertTrue(expenseReportSteps.getExpenseReportView().getCount() > currentCount,
-                "The row has not been added");
-        Assert.assertEquals(expenseReportSteps.getExpenseReportView().getCount(), expectedCount,
-                "Incorrect number of rows");
+        for (int i = 0; i < maxElementsToAdd; i++) {
+            expenseReportView.touchAddButton();
+        }
     }
 
-    @Test
+    @Test(groups = "withoutAddedElements")
     @Tag("View")
     @Owner("Paweł Aksman")
-    @Description("Checking whether a row is removed after touching on the row")
-    @Story("Touching on the row")
-    public void removingRow() {
+    @Description("Adding an element")
+    public void addingElements() {
 
-        int numberOfRows = 2;
-
-        DynamicList.addRows(expenseReportSteps.getExpenseReportView(), numberOfRows);
-
-        int currentCount = expenseReportSteps.getExpenseReportView().getCount();
-        int expectedCount = currentCount - 1;
-
-        expenseReportSteps.tapOnRow(1);
-
-        Assert.assertTrue(expenseReportSteps.getExpenseReportView().getCount() < currentCount,
-                "The row has not been removed");
-        Assert.assertEquals(expenseReportSteps.getExpenseReportView().getCount(), expectedCount,
-                "Incorrect number of rows");
+        addRows();
+        Assert.assertEquals(expenseReportView.getCount(), maxElementsToAdd + 1,"Incorrect number of elements");
     }
 
-    @Test
+    @Test(groups = "withAddedElements")
     @Tag("View")
     @Owner("Paweł Aksman")
-    @Description("Verify that the \"Expense Report\" view is closed after touching the \"Back\" button")
-    @Story("Touching the \"Back\" button")
-    public void closingView() {
+    @Description("Removing an element")
+    public void removingElements() {
 
-        expenseReportSteps.tapBackButton();
+        int previousSize = expenseReportView.getCount();
 
+        for (int i = expenseReportView.getCount() - 1; i > maxElementsToRemove; i--) {
+            expenseReportView.touchElement(i);
+        }
+
+        Assert.assertTrue(expenseReportView.getCount() < previousSize ,"The actual number of elements is not less than previous number");
+    }
+
+    @Test(groups = "withoutAddedElements")
+    @Tag("View")
+    @Owner("Paweł Aksman")
+    @Description("The \"Back\" button")
+    public void backButton() {
+
+        expenseReportView.touchBackButton();
         HomeView homeView = new HomeView(getDriver());
 
         try {
